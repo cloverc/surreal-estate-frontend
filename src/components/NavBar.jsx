71758f21d618b342d482b56c92e01f55b7517cfa
Ersel Aker <erselaker@gmail.com>
2018-05-31 19:25:31 +0100

    added search box

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,6 +16,17 @@ const NavigationBar = () => (
           <a className="nav-link" href="#">Add a Property</a>
         </li>
       </ul>
+      <form className="form-inline my-2 my-lg-0">
+        <select
+          className="form-control mr-sm-2"
+          id="exampleFormControlSelect1"
+        >
+          <option value="price-less-than">Price less than</option>
+          <option value="price-higher-than">Price higher than</option>
+        </select>
+        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
     </div>
   </nav>
 );
